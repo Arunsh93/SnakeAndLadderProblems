90f7c32b4c7eaa30e486eaafd403e1d830532428
Arunsh93 <Arunhosur93@gmail.com>
2021-09-23 23:09:14 +0530

    Player Starting Position at Zero

---
+++ SnakeAndLadderProblem/UC1_SinglePlayerStartPosition.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace SnakeAndLadderProblem
+{
+    class UC1_SinglePlayerStartPosition
+    {
+        static void Main(string[] args)
+        {
+            int Position = 0;
+            Console.WriteLine("Player is at Position: " + Position);
+        }
+    }
+}
