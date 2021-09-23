461746e60d1bfd812225d1efa75a25f7e7fc7a9c
Arunsh93 <Arunhosur93@gmail.com>
2021-09-23 23:31:28 +0530

    Rolls Dai Using Random Function

---
+++ PlayerRollsDaiUsungRandom/UC2_RollsDaiUsingRandom.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace PlayerRollsDaiUsungRandom
+{
+    class UC2_RollsDaiUsingRandom
+    {
+        static void Main(string[] args)
+
+        {
+            int Position = 0;
+            int rolls = 0;
+            Console.WriteLine("Player is at Position: " + Position);
+
+            Random random = new Random();
+
+            while(true)
+            {
+                rolls = random.Next(7);
+                if (rolls != 0)
+                    break;
+            }
+            Console.WriteLine(rolls);
+        }
+    }
+}
